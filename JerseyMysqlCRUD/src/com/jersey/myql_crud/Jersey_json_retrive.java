d2b17da77b00293754e415c0bdcce1e5b2001530
unknown <bhanuprathap.career1@gail.com>
2015-09-04 19:28:16 +0530

    post,put,delte

--- JerseyMysqlCRUD/src/com/jersey/myql_crud/Jersey_json_retrive.java
+++ JerseyMysqlCRUD/src/com/jersey/myql_crud/Jersey_json_retrive.java
@@ -9,15 +9,19 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+
+import org.codehaus.jettison.json.JSONObject;
 
 @Path("/reqData")
 public class Jersey_json_retrive {
@@ -44,37 +48,56 @@ public class Jersey_json_retrive {
 		//System.out.println("list: "+list.size());
 		return list;
 	}
-	@GET
-	@Path("/create{id}/{name}/{salary}/address")
-	public void InserteEmployee(@PathParam("id")int id,@PathParam("name")String name,@PathParam("salary")int salary,@PathParam("address") 
-	String address) throws Exception{
+	@POST
+	@Path("/add")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void InserteEmployee(Employee emp) throws Exception {
+		System.out.println("employee: "+emp.toString()	);
 		Connection conn=ConnectionObj.getConnection();
-		PreparedStatement pst=conn.prepareStatement("INSERT INTO `emp_data`.`employee`(`id`,`name`,`salary`,`address`)VALUES(?,?,?,?");
-		pst.setInt(0, id);
-		pst.setString(1,name);
-		pst.setInt(2, salary);
-		pst.setString(3, address);
-		Boolean b=pst.execute();
-		if(!b){
+		PreparedStatement pst=conn.prepareStatement("INSERT INTO employee(id,name,salary,address) VALUES(?,?,?,?)");
+		pst.setInt(1, emp.getId());
+		pst.setString(2,emp.getName());
+		pst.setInt(3, emp.getSalary());
+		pst.setString(4, emp.getAddress());
+		int b=pst.executeUpdate();
+	if(b!=0){
 			System.out.println("record not inserted");
 		}
 	}
-	public static void main(String[] args) throws Exception {
-		Connection conn=ConnectionObj.getConnection();
-		String sql="Select * from employee where name = ?";
-		PreparedStatement stmt=conn.prepareStatement(sql);
-		stmt.setString(1, "ramu");
-		ResultSet rs=stmt.executeQuery();
-		Employee emp=new Employee();
-		if (rs.next()){
-			
-			emp.setId( rs.getInt(1));
-			emp.setName(rs.getString(2));
-			emp.setSalary(rs.getInt(3));
-			emp.setAddress(rs.getString(4));
-			//list.add(emp);
-		}
+	@PUT
+	@Path("/putdata")
+	public void UpdateEmployee(String input) throws Exception {
+		System.out.println(" location: "+ input);
 		
-		System.out.println(emp);
+		JSONObject json = new JSONObject(input);
+		
+		Connection conn=ConnectionObj.getConnection();
+		PreparedStatement pst=conn.prepareStatement("UPDATE emp_data.employee "+
+														"SET address =? "+
+														"WHERE id = ?;");
+		pst.setString(1, json.getString("address"));
+		pst.setInt(2,json.getInt("id"));
+		int b=pst.executeUpdate();
+	if(b!=0){
+			System.out.println("record not inserted");
+		}
 	}
+	
+	@DELETE
+	@Path("/deletedata")
+	public void DeleteEmployee(String input) throws Exception {
+		System.out.println(" location: "+ input);
+		
+		JSONObject json = new JSONObject(input);
+		
+		Connection conn=ConnectionObj.getConnection();
+		PreparedStatement pst=conn.prepareStatement("delete from emp_data.employee "+
+														"WHERE id = ?;");
+		pst.setInt(1,json.getInt("id"));
+		int b=pst.executeUpdate();
+	if(b!=0){
+			System.out.println("record not inserted");
+		}
+	}
+	
 }
